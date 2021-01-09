7613ecb786c95f3bf0ae1df06a27d5ecffd78277
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2021-01-09 12:27:20 +0100

    Updated documentation

--- doc/ArduForgeIndex.h
+++ doc/ArduForgeIndex.h
@@ -3,9 +3,11 @@
 /** \mainpage ArduForge: A library of classes to use with Arduino compatible microcontrollers (Uno/Nano/ESP32/...)
  * 
  * \section Introduction
+ * The Arduino Forge (ArduForge) is intended to build a collection of classes to handle common, popular, consumer grade devices. Every class features an extensive description and documentation as well as an exemplary wiring schematic and example scripts. Hence the main audience are novice users in electronics and students. 
  * 
+ * The source code is available on github: https://github.com/CrossForge/ArduForge
  * 
- * \section Upcoming features and classes
+ * \section NextFeatures Upcoming features and classes
  * \todo ESP32 Cam module
  * \todo BTLE (HC05)
  * \todo Stepper motor
@@ -13,7 +15,7 @@
  * \todo PIR sensor
  * \todo NFC
  * \todo 16 channel Touch sensor
- * \todo BME280 (Barometer module)
+ * \todo BME280 (Barometer and temperature module)
  * 
  */
 
